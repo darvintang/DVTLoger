da0801d2827820fc8c001cc1aef50edf2bc50214
xtinput <input@tcoding.cn>
2021-09-19 20:19:11 +0800

    修改版本限制

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "DVTLoger",
+    platforms: [.macOS(.v10_10),
+                .iOS(.v10),
+                .tvOS(.v9),
+                .watchOS(.v2)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
